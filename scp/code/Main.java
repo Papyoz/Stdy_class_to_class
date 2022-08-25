eec3eb3336e2a21a0acb130e41c3e972c0067dca
Богданов Никита Андреевич (973490) <111503973+Papyoz@users.noreply.github.com>
2022-08-25 03:23:59 +0300

    Update Main.java

--- scp/code/Main.java
+++ scp/code/Main.java
@@ -1,2 +1,32 @@
-package code;public class Main {
+package code;
+
+public class Main {
+    public static void main(String[] args) {
+        BOOK B = new BOOK("Война и мир");
+        B.book_called();
+    }
 }
+    class BOOK{
+        private String title;
+        private NAME name;
+
+        {
+            title = "Произведение ";
+            name = new NAME();
+            System.out.println("Создан экземпляр класса author");
+        }
+        BOOK(){}
+        BOOK(String title){
+            this.title = this.title + title;
+        }
+
+        void book_called(){
+            name.book_called();
+        }
+
+        class NAME{
+            void book_called(){
+                System.out.println(title + " под авторством...");
+            }
+        }
+    }
